ee6c852f8ad971eea0bec992f1433144233a388b
luanhroliveira <luanhigor98@gmail.com>
2020-04-02 21:20:02 -0300

    Implementando classe Cidade

---
+++ src/projeto/java/Programa/Programa.java
@@ -0,0 +1,15 @@
+package projeto.java.Programa;
+
+import projeto.java.modelo.entidades.Cidade;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Cidade cidade = new Cidade(1, "Araruna", "Testando classe cidade");
+		
+		System.out.println(cidade);
+		
+	}
+
+}
---
+++ src/projeto/java/modelo/entidades/Cidade.java
@@ -0,0 +1,80 @@
+package projeto.java.modelo.entidades;
+
+import java.io.Serializable;
+
+public class Cidade implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	private Integer idCidade;
+	private String nome;
+	private String descricao;
+	
+	public Cidade() {
+	}
+
+	public Cidade(Integer idCidade, String nome, String descricao) {
+		this.idCidade = idCidade;
+		this.nome = nome;
+		this.descricao = descricao;
+	}
+
+	public Integer getIdCidade() {
+		return idCidade;
+	}
+
+	public void setIdCidade(Integer idCidade) {
+		this.idCidade = idCidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idCidade == null) ? 0 : idCidade.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (idCidade == null) {
+			if (other.idCidade != null)
+				return false;
+		} else if (!idCidade.equals(other.idCidade))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Cidade [idCidade=" + idCidade + ", nome=" + nome + ", descricao=" + descricao + "]";
+	}
+}
