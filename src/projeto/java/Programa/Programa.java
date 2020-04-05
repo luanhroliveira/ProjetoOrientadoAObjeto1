ae8b8bc40fbe9284eaddc5bb2b417da0e7b1760b
luanhroliveira <luanhigor98@gmail.com>
2020-04-05 13:53:23 -0300

    Configurando JavaFX

--- src/projeto/java/Programa/Programa.java
+++ src/projeto/java/Programa/Programa.java
@@ -1,44 +1,28 @@
 package projeto.java.Programa;
 
-import java.util.List;
+import java.io.IOException;
 
-import projeto.java.modelo.dao.CidadeDao;
-import projeto.java.modelo.dao.DaoFactory;
-import projeto.java.modelo.entidades.Cidade;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Programa {
+public class Programa extends Application {
+
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource("/projeto/java/gui/View.fxml"));
+			Scene scene = new Scene(parent);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) {
-		
-		CidadeDao cidadeDao = DaoFactory.createCidadeDao();
-		
-		Cidade cidade = new Cidade(1, "Araruna", "Testando classe cidade");
-		
-		System.out.println(cidade);
-		
-		System.out.println("*****Testando insert*****");
-		Cidade newCidade = new Cidade(null, "Araruna", "Teste 1");
-		cidadeDao.insert(newCidade);
-		System.out.println("Insert executado!");
-		
-		System.out.println("*****Testando busca por Id*****");
-		cidade = cidadeDao.buscaPorId(1);
-	    System.out.println(cidade);
-	    
-	    System.out.println("*****Testando delete por Id*****");
-		cidadeDao.deletePorId(1);
-		System.out.println("Delete executado!");
-		
-		System.out.println("*****Testando update*****");
-		cidadeDao.buscaPorId(1);
-		cidade.setDescricao("Teste UPDATE");
-		cidadeDao.update(cidade);
-		System.out.println("Update executado!");
-		
-		System.out.println("*****Testando busca completa*****");
-		List<Cidade> cidadeList = cidadeDao.buscaCompleta();
-		for(Cidade cid : cidadeList) {
-			System.out.println(cid);
-		}
+		launch(args);
 	}
 }
